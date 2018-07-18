b92929d8b0f944407f14c933e4e7d5a6bc028a9c
ljs <1294851990@qq.com>
2018-07-18 14:58:43 +0800

    itertools_1.py

---
+++ base2/itertools_1.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+"""
+@project = 0715-1
+@file = itertools_1
+@author = Liangjisheng
+@create_time = 2018/7/16 0016 上午 11:40
+"""
+# itertools.accumulate      累加
+import itertools
+x = itertools.accumulate(range(10))
+print(list(x))
+
+
